681ef2d71f027d856fdddbe6088a75fea1d025ad
flying <1690206333@qq.com>
2018-06-10 18:58:48 +0800

    6.10

---
+++ old boy/day2/byte_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-06-10 16:29:07
+# @Author  : flying
+
+msg = "喜欢一个人"
+msg_1 = msg.encode(encoding='utf-8')
+msg_2 = msg_1.decode(encoding='utf-8')
+
+print(msg_1,msg_2)
---
+++ old boy/day2/sys_mod.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-06-10 12:53:55
+# @Author  : flying
+'''
+import sys
+
+#print(sys.path) #打印环境变量
+print(sys.argv) #相对路径
+'''
+
+'''
+import os
+
+#cmd = os.system("dir") #执行命令，不保存结果
+cmd = os.popen("dir").read()
+print("-->",cmd)
+os.mkdir("new_dir")
+
+'''
+import login
