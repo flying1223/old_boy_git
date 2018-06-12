48bf7bac3429027acbcd8cb0998a62cb0cb969c4
tongxinli.li <tongxinli.li@qunar.com>
2018-06-12 18:49:13 +0800

    0612

---
+++ old boy/day2/operation2/shopping_cart.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-06-11 21:27:17
+# @Author  : flying
+
+salay = int(input("请输入你的工资："))
+
+shop_list = [[1,'Iphone',5800],[2,'Mac Pro',12000],[3,'Starbuck Latte',31],[4,'Alex python',81],[5,'Bike',800]]
+for i in shop_list:
+    print("商品：",i)
+
+num = int(input("请输入想要购买商品的编号："))
+
+
+
+
+
