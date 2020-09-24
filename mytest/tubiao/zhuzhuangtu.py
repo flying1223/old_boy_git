5754d4039533a0c44e05c516fdff69b26a82d69d
tongxin <1690206333@qq.com>
2020-09-25 00:01:30 +0800

    update

--- mytest/tubiao/zhuzhuangtu.py
+++ mytest/tubiao/zhuzhuangtu.py
@@ -3,18 +3,17 @@
 # @Date    : 2020-09-14
 # Author   : flying
 
-from pyecharts import Bar
+from pyecharts import Bar #导入bar模块
 
-movieName = ["战狼2","速度","功夫瑜伽","西游伏妖篇","变形金刚5","摔跤吧"]
-movieMoney = [56.01, 26.94, 17.53, 16.49, 15.45, 12.96]
+attr = ["音响", "电视", "相机", "Pad", "手机", "电脑"] #设置x轴数据
+v1 = [5, 20, 36, 10, 75, 90] #第一组数据
+v2 = [10, 25, 8, 60, 20, 80] #第二组数据
+bar = Bar("1.1.柱状图数据堆叠示例","我是副标题") #实例一个柱状图#
+bar.use_theme("macarons")  # 指定图表显示的主题风格，后面会讲
+bar.add("京东", attr, v1, mark_point=['average'], is_stack=True) #用add函数往图里添加数据并设置is_stack为堆叠
+bar.add("淘宝", attr, v2, mark_point=['max'], is_stack=True) #mark_point标记min,max,average, mark_line标记线
 
-print(len(movieName), len(movieMoney))
-bar1 = Bar(title="某年内地电影票房前20的电影", subtitle="这是一个子标题")
-# 添加图表的数据, 或者配置信息
-bar1.add("电影信息",movieName, movieMoney)
-
-# 默认情况下会生成一个render.html文件
-bar1.render()
+bar.render("1.1.柱状图数据堆叠示例.html") #保存为html类型
 
 
 
